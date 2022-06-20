Fixed border issue with sky map binning, added full pipeline functions, and added a way to derive coadd scale factor for proper flux scaling

syntheticImages/fakes/insert_fakes.py
@@ -146,8 +146,8 @@ class ImageBuilder():
                                                      np.abs(self.polyDict[key])
                                                      * fracDisplacement)
                     # Flip a coin to swap orientation on some cross terms
-                    # if key[1] != key[3]:
-                    if (key[1] != '0') & (key[3] != '0'):
+                    if key[1] != key[3]:
+                    # if (key[1] != '0') & (key[3] != '0'):
                         coin = rng.integers(0, 2)
                         if coin:
                             self.polyDict[key] = -self.polyDict[key]